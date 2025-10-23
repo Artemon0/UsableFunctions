8b1d551f9bb518169b936f93bd141e95cc78e064
Artem Onyshchenko <artemon888.com@gmail.com>
2025-10-23 18:23:35 +0300

    Updated ver

--- UFuse.py
+++ UFuse.py
@@ -1,5 +1,2 @@
 from UsableFunctions.UF import UsableFunctions as u
 
-res = u.tocken("22kc6MhrlSrzpJSbLcSzggFFOYoitIjWreePqqv7U1igIiAZ1PmlxHCgS5v4",
-               "22kc6MhrlSrzpJSbLcSzggFFOYoitIjWreePqqv7U1igIiAZ1PmlxHCgS5v4")
-print(res)
--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup, find_namespace_packages
 
 setup(
     name="UsableFunctions",
-    version="1.5.2",
+    version="1.5.3",
     description="Very useful functions",
     url="https://github.com/Artemon0/UsableFunctions.git",
-    download_url='https://github.com/Artemon0/UsableFunctions/archive/refs/tags/1.5.2.tar.gz',
+    download_url='https://github.com/Artemon0/UsableFunctions/archive/refs/tags/1.5.3.tar.gz',
     author="Artem Onyshchenko",
     author_email="artemon888.com@gmail.com",
-    license="MIT",
+    license="GPL v3.0",
     packages=find_namespace_packages(),
     install_requires=["tqdm", "PyInstaller", "pygame"],
     # entry_points={"console_scripts": ["quest = UsableFunctions.UF:quest"]},
